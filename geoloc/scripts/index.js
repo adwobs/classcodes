4a0340d19122e53f059bd3f3a9377704febb8491
Adwoba Bota <adwoba.bota@ashesi.edu.gh>
2016-10-30 16:50:55 +0000

    Update index.js

--- geoloc/scripts/index.js
+++ geoloc/scripts/index.js
@@ -1,4 +1,19 @@
-﻿(function() {
+(function() {
+
+		//barcode scanner 
+	document.getElementById("barcode").onclick= function(){
+		cordova.plugins.barcodeScanner.scan(result,error);
+	}
+	cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      });
 
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
